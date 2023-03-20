660257daf7bda32de6afaf7fd5f7928119a0e953
Michael Skelton <codingo@protonmail.com>
2023-03-20 18:28:05 +1000

    Updated for multithreading

--- word_frequency.go
+++ word_frequency.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"sort"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -22,6 +23,7 @@ func main() {
 	targets := flag.String("t", "", "targets file (newline per webpage to load)")
 	exclude := flag.String("e", "", "exclude file (newline per word to exclude)")
 	number := flag.Int("n", 10, "the number of most common words to output")
+	threads := flag.Int("threads", 10, "the number of threads to use")
 
 	flag.Parse()
 
@@ -42,119 +44,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	for _, url := range urls {
-		wordList, err := extractWords(url)
-		if err != nil {
-			fmt.Println("Error extracting words:", err)
-			continue
+	urlsChan := make(chan string)
+	go func() {
+		for _, url := range urls {
+			urlsChan <- url
 		}
+		close(urlsChan)
+	}()
 
-		wordMap := make(map[string]int)
-		for _, word := range wordList {
-			word = strings.ToLower(word)
-			if !excludedWords[word] {
-				wordMap[word]++
+	var wg sync.WaitGroup
+	for i := 0; i < *threads; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for url := range urlsChan {
+				wordList, err := extractWords(url)
+				if err != nil {
+					fmt.Println("Error extracting words:", err)
+					continue
+				}
+
+				wordMap := make(map[string]int)
+				for _, word := range wordList {
+					word = strings.ToLower(word)
+					if !excludedWords[word] {
+						wordMap[word]++
+					}
+				}
+
+				frequencies := createFrequencies(wordMap)
+
+				fmt.Printf("\nResults for %s:\n", url)
+				printFrequencies(frequencies, *number)
 			}
-		}
-
-		frequencies := createFrequencies(wordMap)
-
-		fmt.Printf("\nResults for %s:\n", url)
-		printFrequencies(frequencies, *number)
-	}
-}
-
-func loadExcludedWords(filename string) (map[string]bool, error) {
-	excludedWords := make(map[string]bool)
-
-	if filename == "" {
-		return excludedWords, nil
+		}()
 	}
 
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		excludedWords[strings.ToLower(scanner.Text())] = true
-	}
-
-	return excludedWords, nil
-}
-
-func loadURLs(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	urls := []string{}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		url := scanner.Text()
-		urls = append(urls, url)
-	}
-
-	return urls, nil
-}
-
-func createFrequencies(wordMap map[string]int) []WordFrequency {
-	frequencies := []WordFrequency{}
-
-	for word, count := range wordMap {
-		frequencies = append(frequencies, WordFrequency{word, count})
-	}
-
-	sort.Slice(frequencies, func(i, j int) bool {
-		return frequencies[i].count > frequencies[j].count
-	})
-
-	return frequencies
-}
-
-func printFrequencies(frequencies []WordFrequency, number int) {
-	count := 0
-	for i := 0; count < number && i < len(frequencies); i++ {
-		word := strings.TrimSpace(frequencies[i].word)
-		if word == "" {
-			continue
-		}
-
-		fmt.Printf("%s:%d\n", word, frequencies[i].count)
-		count++
-	}
-}
-
-func extractWords(url string) ([]string, error) {
-	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-		url = "https://" + url
-	}
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
-	if err != nil {
-		return nil, err
-	}
-
-	words := []string{}
-	doc.Find("body").Each(func(_ int, s *goquery.Selection) {
-		text := strings.TrimSpace(s.Text())
-		words = append(words, strings.Split(text, " ")...)
-	})
-
-	return words, nil
+	wg.Wait()
 }
